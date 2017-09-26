c0476386c48e40b132595881b445e81ee321ac6a
Wyatt Childers <wchilders@nearce.com>
2017-09-26 17:59:04 -0400

    Fixed world list

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,7 @@ struct WorldResult {
 }
 
 fn process_world(world_id: isize, average_ping: f32) -> WorldResult {
-  WorldResult { 
+  WorldResult {
     world_id: world_id,
     average_ping: average_ping
   }
@@ -55,19 +55,28 @@ fn print_results(world_results: &mut Vec<WorldResult>) {
 }
 
 fn main() {
-  let worlds = vec![1isize,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,65,66,67,68,69,70,73,74,75,76,77,78,81,82,83,84,85,86,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,131,132,132,133,134,135,136,137,138,139,140];
- 
+  let worlds = vec![
+    1isize, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16, 17,
+    18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32,
+    33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 48,
+    49, 50, 51, 52, 53, 54, 56, 57, 58, 59, 60, 61, 62, 63, 64,
+    65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 76, 77, 78, 79, 80,
+    81, 82, 83, 84, 85, 86, 87, 88, 89, 91, 92, 96, 97, 98, 99,
+    100, 103, 104, 105, 106, 108, 114, 115, 116, 117, 119, 120,
+    123, 124, 134, 135, 136, 137, 138, 139, 140, 141
+  ];
+
   let avg_regex = Regex::new(r"min/avg/max/mdev = ([0-9\.]*)/([0-9\.]*)/([0-9\.]*)/([0-9\.]*)").unwrap();
   let mut world_results = Vec::new();
 
   for world_id in worlds.iter() {
-    let target_server = &format!("world{}.runescape.com", world_id);
-    let ping_result = Command::new("ping").args(&["-c", "3", target_server]).output().expect("failed to execute ping");
+    let target_server = format!("world{}.runescape.com", world_id);
+    let ping_result = Command::new("ping").args(&["-c", "3", &target_server]).output().expect("failed to execute ping");
     let ping_text = String::from_utf8_lossy(&ping_result.stdout);
-    
+
     for capture in avg_regex.captures_iter(&ping_text) {
       let ping = capture[2].parse::<f32>().unwrap();
-      world_results.push(process_world(*world_id, ping));  
+      world_results.push(process_world(*world_id, ping));
     }
 
     print_current_best(&mut world_results);
@@ -77,4 +86,3 @@ fn main() {
 
   print_results(&mut world_results);
 }
-
